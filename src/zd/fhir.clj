674df20e814aff961f546fec117d1ad79080cae4
niquola <niquola@gmail.com>
2022-02-08 17:41:26 +0300

    init

---
+++ src/zd/fhir.clj
@@ -0,0 +1,25 @@
+(ns zd.fhir
+  (:require [zd.core]
+            [zen.core]))
+
+(defonce dtx (atom nil))
+
+(defn stop-docs []
+  (when-let [dtx @dtx] 
+    (zd.core/stop dtx)))
+
+(defn start-docs []
+  (stop-docs)
+  (let [pth (str (System/getProperty "user.dir") "/docs")
+        ztx (zen.core/new-context {:zd/paths [pth] :paths [pth]})]
+    (zd.core/start ztx {:port 3030})
+    (reset! dtx ztx))
+  :ok)
+
+(comment
+
+
+  (start-docs)
+
+
+  )
